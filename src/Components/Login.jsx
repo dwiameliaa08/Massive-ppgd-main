b2f69a61b8d597e9f2b07daf26c51fa1c5b25b28
dwiameliaa08 <dwiamelia065@gmail.com>
2023-12-12 23:07:57 +0700

    baru

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -4,7 +4,7 @@ const Login = () => {
   return (
     <div className="w-full min-h-screen">
       <div className="w-full flex flex-col items-center justify-center my-10">
-        <p className="w-3/4 h-1/2 bg-slate-200">Login</p>
+        <p className="w-3/4 h-1/2 bg-slate-200">apayaaaaaaaaaaaaaaa</p>
       </div>
     </div>
   );
